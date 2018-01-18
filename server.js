d87e6a3661f3c35a0baad290333ca189c219c806
aditya <adityakulai93@gmail.com>
2018-01-18 09:58:16 -0500

    Add projects page

--- server.js
+++ server.js
@@ -58,6 +58,11 @@ app.get('/about', (req,res) => {
   });
 });
 
+app.get('/projects' , (req,res) => {
+  res.render('projects.hbs', {
+    pageTitle : 'PROJECTS'
+  });
+});
 app.get('/bad' , (req,res) => {
   res.send({
       errorMessage : 'Unable to send request'
