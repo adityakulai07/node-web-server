b367deef3c7974ec4406cb9ec628e58723f67975
aditya <adityakulai93@gmail.com>
2018-01-18 09:42:08 -0500

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -28,9 +29,9 @@ app.use((req,res,next) => {
   next();
 });
 
-app.use((req,res,next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs');
+// });
 //routing to the root /
 app.get('/', (request, respond) => {
   //below sent text data
@@ -62,6 +63,6 @@ app.get('/bad' , (req,res) => {
       errorMessage : 'Unable to send request'
   });
 });
-app.listen(3000, () => {
-  console.log('server is up and running on port 3000')
+app.listen(port, () => {
+  console.log(`server is up and running on port ${port}`);
 });
